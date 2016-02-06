00b8f2ccb6c9643a05076869514ec09cf6923c70
csilbergleith <git@10ringaz.com>
2016-02-06 14:44:20 -0700

    Capture result set to either of 2 tables
    This version sets the format for being able to capture multiple result
    sets. This version limits to 2 capture tables @rsTable1 and @rsTable2,
    but can easily be extended. A stored procedure is used to provide
    defaults for omitted parameters

--- genericProcCall/ResultSetCapture.cs
+++ genericProcCall/ResultSetCapture.cs
@@ -12,9 +12,9 @@ public partial class StoredProcedures
 {
     [Microsoft.SqlServer.Server.SqlProcedure]
     public static void ResultSetCapture 
-       (SqlString callXML, SqlString Command, 
+       (SqlString Command, 
         SqlString rsTable1, SqlString rsColumnList1, 
-        SqlString? rsTable2, SqlString? rsColumnList2
+        SqlString rsTable2, SqlString rsColumnList2
        )
     {
         // Storage for the parameters: 
@@ -28,9 +28,16 @@ public partial class StoredProcedures
 
         // Could have a series of cmd.rsTable.Add(...) for each result set if the rsTableN is not null
         // Add the result set table name, column list and a result set sequence number
-        cmd.rsTable.Add(new ResultTables { resultSetSeq = 1, tableName = rsTable1.ToString(), columnList = rsColumnList1.ToString() });
-
-        cmd.rsTable.Add(new ResultTables { resultSetSeq = 2, tableName = rsTable2.ToString(), columnList = rsColumnList2.ToString() });
+        if ( rsTable1.ToString() != "")
+        {
+            cmd.rsTable.Add(new ResultTables { resultSetSeq = 1, tableName = rsTable1.ToString(), columnList = rsColumnList1.ToString() });
+        }
+        
+        if  ( rsTable2.ToString() != "" )
+        {
+            cmd.rsTable.Add(new ResultTables { resultSetSeq = 2, tableName = rsTable2.ToString(), columnList = rsColumnList2.ToString() });
+        }
+       
 
         // the column list; null; empty or * means all; otherwise a csv list
         //***** just store it here
@@ -41,6 +48,11 @@ public partial class StoredProcedures
 
         // Build a dataset with the output tables
         DataSet dsTargetTables = CommandCallUtilities.getTargetTableMetaData(cmd);
+
+        // At this point we have the result set of the command and the meta data for the 
+        // target tables. 
+        // Use the columnList choose the columns and meta data from the result set
+        // and then add any columns that are missing to the target table
 
         // Map the columns of the result set to the columns of the output table; 
         bool result = CommandCallUtilities.mapResultsToOutputTables(CommnandResults, dsTargetTables, cmd);
